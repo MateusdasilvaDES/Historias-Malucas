aa3b5e060e8adac56f90781ebaada2bd174500f6
MATEUS DA SILVA <mateus_silva44@estudante.sesisenai.org.br>
2025-02-12 17:24:32 -0300

    novas

---
+++ Historias Malucas/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Corpo from './componentes/Corpo'
+import NavBar from './componentes/NavBar'
+
+function App() {
+  
+  return (
+    <>
+      <NavBar/>
+      <Corpo/>
+    </>
+  )
+}
+
+export default App
---
+++ Historias Malucas/src/componentes/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <div className="NavBar">
+        <img className="NavBar_imagem" src="brainstorm.png" alt="" />
+        <h1 className="NavBar_titulo">Historias Malucas</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
