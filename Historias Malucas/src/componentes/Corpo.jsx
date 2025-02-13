516e7d625f6444826717714028bfe1ef7171dfaa
MateusdasilvaDES <mateus_silva44@estudante.sesisenai.org.br>
2025-02-13 15:40:21 -0300

    novas

--- Historias Malucas/src/componentes/Corpo.jsx
+++ Historias Malucas/src/componentes/Corpo.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import './Corpo.css'
+import { useState } from 'react';
+import './Corpo.css';
 
-const Corpo = () => { 
+const Corpo = () => {
+    const [resultado, setResultado] = useState('');
+    const array_imagens = ['👽', '👻', '💩', '👀', '🦴', '🧠', '👮‍♂️', '🧑‍🎓', '🦸‍♂️', '🤰', '👰‍♂️'];
 
-    const [resultado, setResultado] = useState('')
-    const array_imagens = ['😎','👽','👻','💩','👀','🦴','🧠','👮‍♂️','🧑‍🎓','🦸‍♂️','🤰','👰‍♂️']
-
-    function trocar(){
-      
-        const random = Math.floor(Math.random() * array_imagens.Length)
-        setResultado(array_imagens[random])
-        alert(resultado)
+    function trocar() {
+        const random = Math.floor(Math.random() * array_imagens.length);
+        setResultado(array_imagens[random]);
     }
 
-  return (
-    <div className='container-corpo'>
-     <div className='container_regras'>
-        <div className='regras'>
-          <h2>Regra: O usuario escolhe a imagem clicando no botão, logo depois inventa uma historia sobre a imagem.</h2>
+    return (
+        <div className='container-corpo'>
+            <div className='container_regras'>
+                <div className='regras'>
+                    <h2>Regra: O usuario escolhe a imagem clicando no botão, logo depois inventa uma historia sobre a imagem.</h2>
+                </div>
+            </div>
+            <div className='container-corpo_imagem'>
+                <div className='div_imagem'>
+                    <span className='imagem'>
+                        {resultado || "❓"}
+                    </span>
+                </div>
+            </div>
+            <div className='div_button'>
+                <button className='button_trocar' onClick={trocar}>Trocar imagem</button>
+            </div>
         </div>
-     </div>
-     <div className='container-corpo_imagem '>
-      <div className='div_imagem'>
-        <img className='imagem' src={resultado || "interrogation-mark.png"} alt="" />
-      </div>
-     </div>
-     <div className='div_button'>
-      <button className='button_trocar' onClick={trocar}>Trocar imagem</button>
-     </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Corpo
+export default Corpo;
